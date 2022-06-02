83dff06b2407db5328f8fff8ec40581bd27d1110
ahjiat <ah_jiat@hotmail.com>
2022-06-02 09:22:05 +0000

    add captcha with cache

--- utility/captcha/captcha.go
+++ utility/captcha/captcha.go
@@ -1,12 +1,43 @@
 package captcha
 
 import (
+	"github.com/patrickmn/go-cache"
 	"github.com/dchest/captcha"
 	"github.com/google/uuid"
+	"encoding/base64"
 	"net/http"
 	"fmt"
+	"time"
 	"bytes"
 )
+
+var localCache *cache.Cache
+
+const (
+	NoExpiration time.Duration = cache.NoExpiration
+)
+
+func init() {
+	localCache = cache.New(cache.NoExpiration, 5 * time.Minute)
+}
+
+func Set(key string, value interface{}, t time.Duration) {
+	localCache.Set(key, value, t)
+}
+
+func Get[T any](key string) (T, bool) {
+	if value, found := localCache.Get(key); found {
+		return value.(T), true
+	}
+	return *new(T), false
+}
+
+func Delete(key string) {
+	localCache.Delete(key)
+}
+
+
+
 
 func WriteImage(w http.ResponseWriter) (string, string) {
 	var imgValue string
@@ -32,4 +63,21 @@ func GenImage() (string, string, []byte) {
 		imgValue += fmt.Sprintf("%d", d)
 	}
 	return outUUID, imgValue, imgBuf.Bytes()
+}
+
+func GenBase64() (string, string, string) {
+	uuid, digits, imgInByte := GenImage()
+	return uuid, digits, "data:image/png;base64," + base64.StdEncoding.EncodeToString(imgInByte)
+}
+
+func GetBase64SecureImg(expire time.Duration) (string, string) {
+	uuid, digits, base64Img := GenBase64()
+	Set(uuid, digits, expire)
+	return uuid, base64Img
+}
+
+func Verify(uuid string, digits string) bool {
+	value, found := Get[string](uuid); if ! found { return false }
+	if value != digits { return false }
+	return true
 }
